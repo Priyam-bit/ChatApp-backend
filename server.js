02eb7aad555cc610bc9a96c5e9953733c1671afa
priyam <btech10477.19@bitmesra.ac.in>
2022-12-29 17:01:42 +0530

    updated middleware

--- server.js
+++ server.js
@@ -70,10 +70,10 @@ io.on('connection', socket=>{
 if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
+    app.use(cors());
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-    app.use(cors());
 }
 
 const PORT = process.env.PORT || 8000;
