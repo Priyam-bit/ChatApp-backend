607a152c62f1eef47abd8de67ae076736c5188a1
priyam <btech10477.19@bitmesra.ac.in>
2022-12-29 16:23:17 +0530

    updated cors origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const socketRoom = []; //collection of rooms corresponding to each socket
 
 app.use(
     cors({
-      origin: process.env.CORS_ORIGIN,
+      origin: '*',
       credentials: true,
     })
 );
