02cd21078c76c7e4dd1ddf5469076cb5ea1cf45d
priyam <btech10477.19@bitmesra.ac.in>
2022-12-29 16:52:41 +0530

    updated middleware

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const cors = require('cors');
 const rooms = {}; //represents all the rooms
 const socketRoom = []; //collection of rooms corresponding to each socket
 
-app.use(cors());
 
 io.on('connection', socket=>{
     console.log('connected');
@@ -33,29 +32,29 @@ io.on('connection', socket=>{
             socket.to(otherUser).emit('user joined', socket.id);
         }
     });
-
+    
     //send the initial offer of new user to existing user
     socket.on('offer', payload =>{ //{from,target,sdp}
         console.log('offer received');
         io.to(payload.target).emit('offer', payload);
     });
-
+    
     //send the existing user's answer to new user
     socket.on('answer', payload =>{ //{target,sdp}
         console.log('answer sent')
         io.to(payload.target).emit('answer', payload);
     });
-
+    
     //send the ice candidates back and forth to both peers until mutual agreement
     socket.on('ice-candidate', incoming =>{  //{target,candidate}
         io.to(incoming.target).emit('ice-candidate', incoming.candidate);
     });
-
+    
     //handle user leaving the room
     socket.on('disconnect', (reason) => {
         if (reason === "io server disconnect") {
-          // the disconnection was initiated by the server, you need to reconnect manually
-          socket.connect();
+            // the disconnection was initiated by the server, you need to reconnect manually
+            socket.connect();
         }
         const roomID = socketRoom[socket.id];  //id of the room, which the user left
         let room = rooms[roomID];     //the room, which the user left
@@ -72,9 +71,10 @@ if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+    app.use(cors());
+}
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
