c3e3d3ee595cf0439103d7d8a5e00af38b2d37be
priyam <btech10477.19@bitmesra.ac.in>
2022-12-29 18:06:18 +0530

    set to default cors

--- server.js
+++ server.js
@@ -2,25 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const app = express();
-const cors = require('cors');
 const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
 const path = require('path');
-const io = require('socket.io')(http, {
-    handlePreflightRequest: (req, res) => {
-      const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-        "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-    }
-  });
-  
+const cors = require('cors');
 
 const rooms = {}; //represents all the rooms
 const socketRoom = []; //collection of rooms corresponding to each socket
 
+app.use(cors());
 
 io.on('connection', socket=>{
     console.log('connected');
@@ -42,29 +33,29 @@ io.on('connection', socket=>{
             socket.to(otherUser).emit('user joined', socket.id);
         }
     });
-    
+
     //send the initial offer of new user to existing user
     socket.on('offer', payload =>{ //{from,target,sdp}
         console.log('offer received');
         io.to(payload.target).emit('offer', payload);
     });
-    
+
     //send the existing user's answer to new user
     socket.on('answer', payload =>{ //{target,sdp}
         console.log('answer sent')
         io.to(payload.target).emit('answer', payload);
     });
-    
+
     //send the ice candidates back and forth to both peers until mutual agreement
     socket.on('ice-candidate', incoming =>{  //{target,candidate}
         io.to(incoming.target).emit('ice-candidate', incoming.candidate);
     });
-    
+
     //handle user leaving the room
     socket.on('disconnect', (reason) => {
         if (reason === "io server disconnect") {
-            // the disconnection was initiated by the server, you need to reconnect manually
-            socket.connect();
+          // the disconnection was initiated by the server, you need to reconnect manually
+          socket.connect();
         }
         const roomID = socketRoom[socket.id];  //id of the room, which the user left
         let room = rooms[roomID];     //the room, which the user left
@@ -80,11 +71,10 @@ io.on('connection', socket=>{
 if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
-    // app.use(cors());
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
